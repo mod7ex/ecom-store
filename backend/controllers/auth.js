1421dad63f7c9333727c59041a7de386f9014272
Mourad EL CADI <mouradface1998@gmail.com>
2021-11-21 16:57:21 +0100

    working on auth route

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,9 +1,34 @@
+const { User } = require("../models");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, UnauthorizedError } = require("../errors");
+
 let register = async (req, res, next) => {
-      res.send("register User");
+      let user = await User.create(req.body);
+      let token = user.tokenize();
+      res.status(StatusCodes.CREATED).json({ user, token });
 };
 
 let login = async (req, res, next) => {
-      res.send("login User");
+      let { email, password } = req.body;
+
+      if (!email || !password || password.length < 6 || password.length > 32) {
+            throw new BadRequestError("Incorrect password");
+      }
+
+      let user = await User.findOne({ email });
+
+      if (!user) {
+            throw new UnauthorizedError("Incorrect data");
+      }
+
+      let isValid = await user.checkPassword(password);
+
+      if (!isValid) {
+            throw new UnauthorizedError("Incorrect data");
+      }
+
+      let token = user.tokenize();
+      res.status(StatusCodes.OK).json({ user, token });
 };
 
 let logout = async (req, res, next) => {
--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,8 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
+const { BadRequestError } = require("../errors");
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 let userSchema = new mongoose.Schema({
       name: {
@@ -10,6 +14,11 @@ let userSchema = new mongoose.Schema({
       email: {
             type: String,
             required: [true, "Email is required"],
+            unique: true,
+            match: [
+                  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                  "Please enter a valid email",
+            ],
             maxLength: 320,
       },
 
@@ -28,5 +37,37 @@ let userSchema = new mongoose.Schema({
             default: false,
       },
 });
+
+userSchema.post("validate", function () {
+      if (this.password.length < 6 || this.password.length > 32)
+            throw new BadRequestError(
+                  "password length should be between 6 and 32"
+            );
+});
+
+userSchema.pre("save", async function () {
+      // hashing the password
+      let salt = await bcryptjs.genSalt(10);
+      this.password = await bcryptjs.hash(this.password, salt);
+});
+
+userSchema.methods.tokenize = function () {
+      return jwt.sign(
+            {
+                  _id: this._id,
+                  name: this.name,
+                  email: this.email,
+            },
+            process.env.JWT_SECRET,
+            {
+                  expiresIn: process.env.JWT_LIFE_TIME,
+            }
+      );
+};
+
+userSchema.methods.checkPassword = async function (password) {
+      let isValid = await bcryptjs.compare(password, this.password);
+      return isValid;
+};
 
 module.exports = mongoose.model("User", userSchema);
